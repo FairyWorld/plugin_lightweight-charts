8fab77769f9ff9735037b8a25343b2ba19f46217
TradingView <noreply@tradingview.com>
2024-07-10 02:07:45 +0000

    [skip ci] Deploy website on 82bb0a028079663fe9218bf8bd60cc3c7812d1e4 82bb0a028079663fe9218bf8bd60cc3c7812d1e4

--- plugin-examples/assets/12-dde54c39.js
+++
@@ -1,1 +0,0 @@
-import{P as i}from"./lightweight-charts.production-48a3bf4f.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-31dfe4d9.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
