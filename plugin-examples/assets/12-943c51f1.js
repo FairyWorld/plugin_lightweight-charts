0fce0a32796542f44f97ee80b0496bc7dac23a0b
TradingView <noreply@tradingview.com>
2024-10-04 14:02:49 +0000

    [skip ci] Deploy website on fbbe9df832c1dfe5e4a9e0f938c107614d5ea308 fbbe9df832c1dfe5e4a9e0f938c107614d5ea308

--- plugin-examples/assets/12-943c51f1.js
+++
@@ -1,1 +0,0 @@
-import{V as i}from"./lightweight-charts.production-82842647.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-fc2179b1.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
